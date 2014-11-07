ab0f0896a9b8f3830e0def8257c452a382f00049
a226035102 <comeonjiji@gmail.com>
2014-11-07 10:25:08 +0800

    Initial Commit

---
+++ JZPhotoBrowser/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JZPhotoBrowser
+//
+//  Created by Joshua Zhou on 14/11/7.
+//  Copyright (c) 2014年 Joshua Zhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
